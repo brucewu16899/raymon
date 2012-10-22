59ead47952594fee27d083a415ac1fa8c1353105
Remy van Elst <rvanelst@digidentity.eu>
2012-10-22 19:04:41 +0200

    Release v0.0.2

--- client.sh
+++ client.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+#Copyright (c) 2012 Remy van Elst
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
 
-#define the process name of your webserver
-# 
-#apache:
-# SERVICE='httpd'
-#
-#lighttpd
-# SERVICE='lighttpd'
-#
-# nginx
-# SERVICE='nginx'
-#
-# NEtwork Interface you want to use
-iface="{{ iface }}:0"
-# network interface for traffic mon
-iface2="{{ iface2 }}"
+# Network interface for the IP address
+iface="venet0:0"
+# network interface for traffic monitoring (RX/TX bytes)
+iface2="venet0"
 
-ipaddr="{{ ipaddr }}"
+ipaddr="1.2.3.4"
 
 #json start
 echo -n "{"
@@ -95,7 +102,7 @@ echo -n "\", "
 
 # package updates, uncomment for your distro
 echo -n "\"updatesavail\" : \""
-#debian
+#debian/ubuntu
 echo -n `apt-get -s upgrade | awk '/[0-9]+ upgraded,/ {print $1}'`
 #arch
 #echo -n `pacman -Sy 1>/dev/null 2>&1; pacman -Qu | wc -l`
--- stat.php
+++ stat.php
@@ -21,70 +21,43 @@
 */
 
 $hostlist=array(
-                'raymii.org.json' => 'http://stat.raymii.org/stat.json',
-                'raymii.nl.json' => 'http://raymii.nl/stat.json',
-                'vps1.spcs.json' => 'http://vps1.sparklingclouds.nl/stat.json',
-                'vps3.spcs.json' => 'http://vps3.sparklingclouds.nl/stat.json',
-                'vps5.spcs.json' => 'http://vps5.sparklingclouds.nl/stat.json',
-                'vps8.spcs.json' => 'http://vps8.sparklingclouds.nl/stat.json',
-                'vps11.spcs.json' => 'http://vps11.sparklingclouds.nl/stat.json',
-                'vps12.spcs.json' => 'http://vps12.sparklingclouds.nl/stat.json',
-                'vps13.spcs.json' => 'http://vps13.sparklingclouds.nl/stat.json',
-                'vps12.spcs.json' => 'http://vps14.sparklingclouds.nl/stat.json'
+                'example1.org.json' => 'http://example1.org/stat.json',
+                'example2.nl.json' => 'http://example2.nl/stat.json',
+                'special1.network.json' => 'http://special1.network.eu:8080/stat.json',
+                'special2.network.json' => 'https://special2.network.eu/stat.json'
                 );
-$pinglist = array('raymii.nl',
-                  'erasmusmc.nl',
-                  'google.com',
-                  'raymii.org',
+
+$pinglist = array(
+                  'github.com',
+                  'google.nl',
                   'tweakers.net',
+                  'jupiterbroadcasting.com',
+                  'lowendtalk.com',
                   'lowendbox.com' 
                   );
+
+## Set this to "secure" the history saving. This key has to be given as a parameter to save the history.
+$historykey = "8A29691737D";
+
+#the below values set the threshold before a value gets shown in bold on the page.
+# Max updates available
+$maxupdates = "10";
+# Max users concurrently logged in
+$maxusers = "3";
+# Max load.
+$maxload = "2";
+# Max disk usage (in percent)
+$maxdisk = "75";
+# Max RAM usage (in percent)
+$maxram = "75";
+
 #Set this or your logs will fill up your disk.
 date_default_timezone_set('Europe/Amsterdam');
 
-
-?>
-
+## END OF CONFIG
 
 
-<html>
-<head>
-    <title>Stats</title>
-    <!-- bar via http://www.joshuawinn.com/quick-and-simple-css-percentage-bar-using-php/ -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    <script type="text/javascript" src="git/js/prettify.js"></script>                                   <!-- PRETTIFY -->
-    <script type="text/javascript" src="git/js/kickstart.js"></script>                                  <!-- KICKSTART -->
-    <link rel="stylesheet" type="text/css" href="git/css/kickstart.css" media="all" />                  <!-- KICKSTART -->
-    <link rel="stylesheet" type="text/css" href="git/css/style.css" media="all" />                      <!-- CUSTOM STYLES -->
-    <meta http-equiv="refresh" content="300">
-    <script type="text/javascript">
-    <!--
-        $(document).ready(function() {
-            var showText="Show";
-            var hideText="Hide";
-            $(".toggle").prev().append(' (<a href="#" class="toggleLink">'+showText+'</a>)');
-            $('.toggle').hide();
-            $('a.toggleLink').click(function() {
-                if ($(this).html()==showText) {
-                    $(this).html(hideText);
-                }
-                else {
-                    $(this).html(showText);
-                }
-                $(this).parent().next('.toggle').toggle('slow');
-                return false;
-            });
-        });
-        //-->
-    </script>
-    <style type="text/css">
-        .percentbar { background:#CCCCCC; border:1px solid #666666; height:10px; }
-        .percentbar div { background: #28B8C0; height: 10px; }
-    </style>
-</head>
-<body><a id="top-of-page"></a><div id="wrap" class="clearfix">
-<?php
+
 function historystat($bestand,$host) {
     $files=array();
     if ($handle = opendir('./history/')) {
@@ -96,6 +69,7 @@ function historystat($bestand,$host) {
         echo "Error: cannot open direcotry './history' for file $bestand from host $host.";
     }
     rsort($files);
+    
     foreach ($files as $key => $value) {
         $filename = explode(".", $value);
         $amount = count($filename);
@@ -105,20 +79,19 @@ function historystat($bestand,$host) {
         if($bestandnaam == $bestand) {
             $jsonfile = "./history/" . $value;
             if ($jsonopen = file_get_contents($jsonfile)) {
-             if (json_decode($jsonopen, true)) {
+               if (json_decode($jsonopen, true)) {
                 $filedate = date("d.m.Y - H:i:s", $timestamp);
-                echo "<h4>Date: $filedate</h4>\n";
-                echo "<table>";
+                echo "<tr><th>Date:</td><th colspan = \"8\">$filedate</td></tr>\n";
                 linestat($jsonfile,$host);
-                echo "</table><br />";
             } else {
                 echo "Cannot decode json file $jsonfile.";
             }
         } else {
-            echo "Cannot open json file $jsonfile.";
-        }
-    }
-}   
+           echo "Cannot open json file $jsonfile.";
+       }
+   }
+}
+
 }
 
 function shortstat($bestand,$host) {
@@ -148,26 +121,43 @@ function savefile($bestand,$naam){
         $DATEINT=date('i');
         $DATESEC=date('s');
 
-#if (filemtime("${curdir}/${naam}") < (time() - 300)) {
-        if ($DATEINT=="05" || $DATEINT=="10" || $DATEINT=="15" ||  $DATEINT=="20" || $DATEINT=="25" || $DATEINT=="35" || $DATEINT=="40" || $DATEINT=="45" || $DATEINT=="50" || $DATEINT=="55") {
+
+        if ($DATEINT=="05" || $DATEINT=="12" || $DATEINT=="15" ||  $DATEINT=="20" || $DATEINT=="25" || $DATEINT=="35" || $DATEINT=="40" || $DATEINT=="45" || $DATEINT=="50" || $DATEINT=="55") {
             $local_file=file_get_contents($bestand);
             $saved_local_file=file_put_contents("${curdir}/$naam", $local_file);
-            if (md5($local_file) != md5(file_get_contents("${curdir}/${naam}.old"))) {
-                file_put_contents("${naam}.old", $local_file);
-            }
+
         } else {
             $local_file=file_get_contents($naam);
         }
-        if($DATEINT=="00" || $DATEINT=="30"){
-            if($DATESEC == "30" || $DATESEC == "55") {
-                file_put_contents("history/${naam}.${DATETIME}", $local_file);
-            }
-        }
+
+        
     }
 
 }
 
+function savehistory($naam) {
+    $curdir=getcwd();
+    $DATETIME=date('U');
+    if(!is_dir("{$curdir}/history")){
+        mkdir("${curdir}/history") or die("Cannot create history folder. Create it manually and make sure the webserver can write to it.");
+    }
+    $local_file=file_get_contents($naam); 
+    
+    if (!file_put_contents("history/${naam}.${DATETIME}", $local_file)) {
+        echo "File $naam could not be saved in history. Please check directory permissions on directory \'history\'.";
+    }
+
+    
+
+}
+
 function linestat($bestand,$host) {
+    global $maxusers;
+    global $maxupdates;
+    global $maxload;
+    global $maxdisk;
+    global $maxram;
+
     if ($file = file_get_contents($bestand)) {
         if ($json_a = json_decode($file, true)) {
             $closed=0;
@@ -199,74 +189,101 @@ function linestat($bestand,$host) {
                     }
                     ?>
                 </td>
-                <td><?php echo str_replace(",", "",$json_a['Load']); ?></td>
-                <td><?php echo $json_a['Users logged on']; ?></td>
-                <td><?php echo $json_a['updatesavail']; ?></td>
+                <?php
+                if (floatval(str_replace(",", "",$json_a['Load'])) > $maxload) {
+                    echo "<td><strong>".round(floatval(str_replace(",", "",$json_a['Load'])),3)."</strong></td>";
+                } else {
+                    echo "<td>".round(floatval(str_replace(",", "",$json_a['Load'])),3)."</td>";
+                }
+                
+                if (intval($json_a['Users logged on']) > $maxusers) {
+                    echo "<td><strong>".$json_a['Users logged on']."</strong></td>";
+                } else {
+                   echo "<td>".$json_a['Users logged on']."</td>"; 
+                }
+
+                if (intval($json_a['updatesavail']) > $maxupdates) {
+                    echo "<td><strong>".$json_a['updatesavail']."</strong></td>";
+                } else {
+                    echo "<td>".$json_a['updatesavail']."</td>";
+                }
+                ?>
+
                 <td><?php
                 percentagebar($json_a['Disk']['percentage']);
                 echo "<br />";
-                echo $json_a['Disk']['total'] . " <br /> "; 
-                echo $json_a['Disk']['used'] . " <br /> ";
-                echo $json_a['Disk']['free'];
-                
+                echo "T: " . $json_a['Disk']['total'] . " <br /> "; 
+
+                if (intval(str_replace("%", "",$json_a['Disk']['percentage'])) > $maxdisk ) {
+                    echo "<strong>U: " . $json_a['Disk']['used'] . "</strong> <br /> ";
+                    echo "<strong>F: " . $json_a['Disk']['free'] . "</strong>";
+                } else {
+                    echo "U: " . $json_a['Disk']['used'] . " <br /> ";
+                    echo "F: " . $json_a['Disk']['free'];
+                }
                 ?>
-                </td>    
-                <td><?php
-                    $used_ram = $json_a['Total RAM'] - $json_a['Free RAM'];
-                    $value = $used_ram;
-                    $max = $json_a['Total RAM'];
-                    $scale = 1.0;
-                    if (!empty($max)) {
-                        $percent = ($value * 100) / $max;
-                    } else {
-                        $percent = 0;
-                    }
-                    if ($percent > 100) {
-                        $percent = 100;
-                    }
-                    percentagebar(round($percent * $scale));
-                    echo "<br />";
-                    echo $json_a['Total RAM'] . " MB <br /> ";
-                    echo $used_ram . " MB <br /> ";
-                    echo $json_a['Free RAM'] . " MB";
-                    ?>
-                </td>
-                <td><?php
-                    $rxmb=round((($json_a['rxbytes'] / 1024) / 1024));
-                   if ($rxmb < 1024) {
-                        echo $rxmb . " MB";
-                    } elseif ($rxmb < 1024000) {
-                        $rxmb = round(($rxmb / 1024),2);
-                        echo $rxmb . " GB";
-                    } elseif ($rxmb > 1024000) {
-                        $rxmb = round((($rxmb / 1024) / 1024),2);
-                        echo $rxmb . " TB";   
-                    }
-                    
-                    ?>
-                </td>
+            </td>    
             <td><?php
-                $txmb=round((($json_a['txbytes'] / 1024) / 1024));
-                if ($txmb < 1024) {
-                        echo $txmb . " MB";
-                    } elseif ($txmb < 1024000) {
-                        $txmb = round(($txmb / 1024),2);
-                        echo $txmb . " GB";
-                    } elseif ($txmb > 1024000) {
-                        $txmb = round((($txmb / 1024) / 1024),2);
-                        echo $txmb . " TB";   
-                    }
-                ?>
-            </td>
-        </tr>
-    <?php
+            $used_ram = $json_a['Total RAM'] - $json_a['Free RAM'];
+            $value = $used_ram;
+            $max = $json_a['Total RAM'];
+            $scale = 1.0;
+            if (!empty($max)) {
+                $percent = ($value * 100) / $max;
+            } else {
+                $percent = 0;
             }
-        } else {
-            echo "Error decoding JSON stat file for host $host";
+            if ($percent > 100) {
+                $percent = 100;
+            }
+            percentagebar(round($percent * $scale));
+            echo "<br />";
+            echo "T: " . $json_a['Total RAM'] . " MB <br /> ";
+            if (intval(str_replace("%", "",round($percent * $scale))) > $maxram ) {
+                echo "<strong>U: " . $used_ram . " MB </strong><br /> ";
+                echo "<strong>F: " . $json_a['Free RAM'] . " MB</strong>";
+            } else {
+                echo "U: " . $used_ram . " MB <br /> ";
+                echo "F: " . $json_a['Free RAM'] . " MB";
+            }
+            ?>
+        </td>
+        <td><?php
+        $rxmb=round((($json_a['rxbytes'] / 1024) / 1024));
+        if ($rxmb < 1024) {
+            echo $rxmb . " MB";
+        } elseif ($rxmb < 1024000) {
+            $rxmb = round(($rxmb / 1024),2);
+            echo $rxmb . " GB";
+        } elseif ($rxmb > 1024000) {
+            $rxmb = round((($rxmb / 1024) / 1024),2);
+            echo $rxmb . " TB";   
         }
-    } else  {
-        echo "Error while getting stats for host $host from file $bestand";
+
+        ?>
+    </td>
+    <td><?php
+    $txmb=round((($json_a['txbytes'] / 1024) / 1024));
+    if ($txmb < 1024) {
+        echo $txmb . " MB";
+    } elseif ($txmb < 1024000) {
+        $txmb = round(($txmb / 1024),2);
+        echo $txmb . " GB";
+    } elseif ($txmb > 1024000) {
+        $txmb = round((($txmb / 1024) / 1024),2);
+        echo $txmb . " TB";   
     }
+    ?>
+</td>
+</tr>
+<?php
+}
+} else {
+    echo "Error decoding JSON stat file for host $host";
+}
+} else  {
+    echo "Error while getting stats for host $host from file $bestand";
+}
 }
 
 function ping($host, $port, $timeout) { 
@@ -287,48 +304,97 @@ function dosomething($bestand,$host,$actie){
             shortstat($bestand,$parsed_host);
             break;
             case 'historystat':
+            echo "<table>";
             savefile($host,$bestand);
             $parsed_host=parse_url($host,PHP_URL_HOST);
             historystat($bestand,$parsed_host);
+            echo "</table>";
             break;
         }
     }   
 }
 
-?> 
-<div class="col_12">
-    <ul class="tabs left">
-        <li><a href="#tabc1">Overview</a></li>
-        <li><a href="#tabc2">History</a></li>
-    </ul>
-    <div id="tabc1" class="tab-content">
-        <?php 
-        echo "<i>Ping monitor:</i>";
-        foreach ($pinglist as $key => $value) {
-            echo ping("$value",80,5) . ", ";
-        }
-        ?>
-        <h4>Server Status</h4>
-        <?php
-        foreach ($hostlist as $key => $value) {
-            $host=parse_url($value,PHP_URL_HOST);
-            echo "<h5>Host: ${host}</h6>";
-            dosomething($key,$value,"shortstat");
-            echo "<hr class=\'alt1\' />";
-        }
-        ?>
-    </div>
-    <div id="tabc2" class="tab-content"> 
-        <?php
-        foreach ($hostlist as $key => $value) {
-            $host=parse_url($value,PHP_URL_HOST);
-            echo "<p>History for host ${host}</p>\n";
-            echo "<div class=\"toggle\">";
-            dosomething($key,$value,"historystat");
-            echo "</div>";
-        }
-        ?>
-    </div>
-</div>
-</body>
-</html>
+if ($_GET["action"] == "save" && $_GET["key"] == "$historykey") {
+    foreach ($hostlist as $key => $value) {
+        savehistory($key);
+        echo "History for: ".$key . " saved. <br />\n" ;
+    }
+    die("History done.<br /> \n");
+
+}
+
+
+?>
+<html>
+<head>
+    <title>Stats</title>
+    <!-- bar via http://www.joshuawinn.com/quick-and-simple-css-percentage-bar-using-php/ -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <script type="text/javascript" src="git/js/prettify.js"></script>                                   <!-- PRETTIFY -->
+    <script type="text/javascript" src="git/js/kickstart.js"></script>                                  <!-- KICKSTART -->
+    <link rel="stylesheet" type="text/css" href="git/css/kickstart.css" media="all" />                  <!-- KICKSTART -->
+    <link rel="stylesheet" type="text/css" href="git/css/style.css" media="all" />                      <!-- CUSTOM STYLES -->
+    <meta http-equiv="refresh" content="300">
+    <script type="text/javascript">
+    <!--
+    $(document).ready(function() {
+        var showText="Show";
+        var hideText="Hide";
+        $(".toggle").prev().append(' (<a href="#" class="toggleLink">'+showText+'</a>)');
+        $('.toggle').hide();
+        $('a.toggleLink').click(function() {
+            if ($(this).html()==showText) {
+                $(this).html(hideText);
+            }
+            else {
+                $(this).html(showText);
+            }
+            $(this).parent().next('.toggle').toggle('slow');
+            return false;
+        });
+    });
+        //-->
+        </script>
+        <style type="text/css">
+        .percentbar { background:#CCCCCC; border:1px solid #666666; height:10px; }
+        .percentbar div { background: #28B8C0; height: 10px; }
+        </style>
+    </head>
+    <body><a id="top-of-page"></a><div id="wrap" class="clearfix">
+        <div class="col_12">
+            <ul class="tabs left">
+                <li><a href="#tabc1">Overview</a></li>
+                <li><a href="#tabc2">History</a></li>
+            </ul>
+            <div id="tabc1" class="tab-content">
+                <?php 
+                echo "<i>Ping monitor:</i>";
+                foreach ($pinglist as $key => $value) {
+                    echo ping("$value",80,5) . ", ";
+                }
+                ?>
+                <h4>Server Status</h4>
+                <?php
+                foreach ($hostlist as $key => $value) {
+                    $host=parse_url($value,PHP_URL_HOST);
+                    echo "<h5>Host: ${host}</h6>";
+                    dosomething($key,$value,"shortstat");
+                    echo "<hr class=\'alt1\' />";
+                }
+                ?>
+            </div>
+            <div id="tabc2" class="tab-content"> 
+                <?php
+                foreach ($hostlist as $key => $value) {
+                    $host=parse_url($value,PHP_URL_HOST);
+                    echo "<p>History for host ${host}</p>\n";
+                    echo "<div class=\"toggle\">";
+                    dosomething($key,$value,"historystat");
+                    echo "</div>";
+                }
+                ?>
+            </div>
+        </div>
+    </body>
+    </html>
